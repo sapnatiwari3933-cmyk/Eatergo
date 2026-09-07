58da81b1f04c2e8557f41689e0f5a627a8efee2c
sapnatiwari3933-cmyk <sapnatiwari3933@gmail.com>
2026-09-07 20:11:36 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,38 @@
+const categories = [
+    {
+        name: "All",
+        image: "https://images.unsplash.com/photo-1547592180-85f173990554?auto=format&fit=crop&w=300&q=80"
+    },
+    {
+        name: "Pizza",
+        image: "https://images.unsplash.com/photo-1574071318508-1cdbab80d002?auto=format&fit=crop&w=300&q=80"
+    },
+    {
+        name: "Burgers",
+        image: "https://images.unsplash.com/photo-1568901346375-23c9450c58cd?auto=format&fit=crop&w=300&q=80"
+    },
+    {
+        name: "Indian",
+        image: "https://images.unsplash.com/photo-1601050690597-df0568f70950?auto=format&fit=crop&w=300&q=80"
+    },
+    {
+        name: "Chinese",
+        image: "https://images.unsplash.com/photo-1563245372-f21724e3856d?auto=format&fit=crop&w=300&q=80"
+    },
+    {
+        name: "Biryani",
+        image: "https://images.unsplash.com/photo-1589302168068-964664d93dc0?auto=format&fit=crop&w=300&q=80"
+    },
+    {
+        name: "Cafe",
+        image: "https://images.unsplash.com/photo-1509042239860-f550ce710b93?auto=format&fit=crop&w=300&q=80"
+    },
+    {
+        name: "Desserts",
+        image: "https://images.unsplash.com/photo-1551024506-0bccd828d307?auto=format&fit=crop&w=300&q=80"
+    },
+    {
+        name: "Healthy Food",
+        image: "https://images.unsplash.com/photo-1512621776951-a57141f2eefd?auto=format&fit=crop&w=300&q=80"
+    }
+];
